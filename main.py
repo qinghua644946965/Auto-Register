6baecfb3bc4d1e353f1b1ec03ea6ae6e45b22cd0
qinghua644946965 <78455113+qinghua644946965@users.noreply.github.com>
2024-12-27 13:08:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import string
 import random
 import re
 import time
-import ddddocr
 from curl_cffi import requests
 # import requests
 
@@ -15,8 +14,6 @@ from fastapi import FastAPI, Form, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-
-ocr = ddddocr.DdddOcr()
 
 app = FastAPI()
 
@@ -177,150 +174,7 @@ def background_task(input_email):
     # ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
     ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
-    header1 = {
-        "Host": "www.serv00.com",
-        "User-Agent": ua,
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Connection": "keep-alive",
-        # "Referer": "https://www.serv00.com/offer/create_new_account",
-        "Referer": "https://www.serv00.com/offer",
-        "Sec-GPC": "1",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Priority": "u=1",
-    }
-
-    captcha_url = "https://www.serv00.com/captcha/image/{}/"
-    header2 = {
-        "Host": "www.serv00.com",
-        "User-Agent": ua,
-        "Accept": "image/avif,image/webp,*/*",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Sec-GPC": "1",
-        "Connection": "keep-alive",
-        "Referer": "https://www.serv00.com/offer/create_new_account",
-        "Cookie": "csrftoken={}",
-        "Sec-Fetch-Dest": "image",
-        "Sec-Fetch-Mode": "no-cors",
-        "Sec-Fetch-Site": "same-origin",
-        "Priority": "u=4",
-        "TE": "trailers",
-    }
-
-    url3 = "https://www.serv00.com/offer/create_new_account.json"
-    header3 = {
-        "Host": "www.serv00.com",
-        "User-Agent": ua,
-        "Accept": "*/*",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "X-Requested-With": "XMLHttpRequest",
-        "Origin": "https://www.serv00.com",
-        "Connection": "keep-alive",
-        "Referer": "https://www.serv00.com/offer/create_new_account",
-        "Cookie": "csrftoken={}",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "Priority": "u=1",
-    }
-
     
-
-    with requests.Session() as session:
-        logger.info("获取网页信息")
-        resp = session.get(url=url1, headers=header1, impersonate="chrome124")
-        logger.info("~~~~~~~~")
-        logger.info(resp)
-        logger.info("++++++++++")
-        print(resp.status_code)
-        headers = resp.headers
-        content = resp.text
-
-        csrftoken = re.findall(r"csrftoken=(\w+);", headers.get("set-cookie"))[0]
-        # csrftoken = csrftoken or re.findall(r"input type='hidden' name='csrfmiddlewaretoken' value='(\w+)'", content)[0]
-        print("csrftoken", csrftoken)
-        header2["Cookie"] = header2["Cookie"].format(csrftoken)
-        header3["Cookie"] = header3["Cookie"].format(csrftoken)
-
-        captcha_0 = re.findall(r'id=\"id_captcha_0\" name=\"captcha_0\" value=\"(\w+)\">', content)[0]
-        retry = 1
-        while True:
-            time.sleep(random.uniform(2.5, 5))
-
-            usernames = get_user_name()
-            _ = usernames.pop()
-            first_name = _["name"]
-            last_name = _["surname"]
-            username = generate_random_username().lower()
-            input_email = cache["email_v"]
-            email = input_email
-            logger.info(f"{email} {first_name} {last_name} {username}")
-
-
-            logger.info("获取验证码")
-            capt = {}
-            resp = session.get(url=captcha_url.format(captcha_0),
-                             headers=dict(header2, **{"Cookie": header2["Cookie"].format(csrftoken)}), impersonate="chrome124")
-            
-            content = resp.content
-            with open(current_dir+"/static/image.jpg", "wb") as f:
-                f.write(content)
-            for i in range(30):
-                _captcha_1 = ocr.classification(content).lower()
-                if bool(re.match(r'^[a-zA-Z0-9]{4}$', _captcha_1)):
-                    if capt.get(_captcha_1):
-                        capt[_captcha_1] += 1
-                    else:
-                        capt[_captcha_1] = 1
-            if len(capt) == 0:
-                retry = 99
-                captcha_1 = "等待手动"
-                print("自动处理验证码失败")
-                print("开始等待手动id....")
-                continue
-            else:
-                captcha_1 = max(capt, key=capt.get)
-                print("captcha_1", captcha_1, "次数:", capt.get(captcha_1))
-
-            print("captcha_0", captcha_0)
-            cache[input_email] = {"auto": captcha_1}
-
-            print (
-                {
-                    first_name,last_name,username,email,captcha_0,captcha_1
-                }
-            )
-            data = f"csrfmiddlewaretoken={csrftoken}&first_name={first_name}&last_name={last_name}&username={username}&email={quote(email)}&captcha_0={captcha_0}&captcha_1={captcha_1}&question=0&tos=on"
-            time.sleep(random.uniform(0.5, 1.2))
-            logger.info("请求信息")
-            resp = session.post(url=url3, headers=dict(header3, **{"Cookie": header3["Cookie"].format(csrftoken)}),
-                                data=data, impersonate="chrome124")
-            print(resp.status_code)
-            print(resp.text)
-            content = resp.json()
-            if content.get("captcha") and content["captcha"][0] == "Invalid CAPTCHA":
-                captcha_0 = content["__captcha_key"]
-                retry += 1
-                logger.warning("验证码错误，正在重新获取")
-                time.sleep(random.uniform(0.5, 1.2))
-                continue
-            elif content["username"][0] == "Maintenance time. Try again later.":
-                captcha_0 = content["__captcha_key"]
-                retry += 1
-                logger.warning("Maintenance time. Try again later....")
-                time.sleep(random.uniform(0.5, 1.2))
-                continue
-            else:
-                logger.warning("貌似注册成功了~~~~~....")
-                break
-
-    cache[input_email].clear()
-    os.remove("static/image.jpg")
 
 
 if __name__ == '__main__':
